5dc95246544d32c42cb1273537075f686d5afcd8
Ott021 <ot.pacheco@duocuc.cl>
2021-06-17 16:44:05 -0400

    forms

--- MiProyecto/core/urls.py
+++ MiProyecto/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.urls import URLPattern
-from .views import index, contacto
+from .views import index, contacto, listarVehiculos,agregarVehiculo
 
 urlpatterns = [
     path('', index, name="index"),
-    path('contacto/', contacto, name="contacto")
+    path('contacto/', contacto, name="contacto"),
+    path('vehiculo/listar',listarVehiculos, name="listarVehiculos"),
+    path('vehiculo/agregar',agregarVehiculo,name="agregarVehiculo")
 ]
--- MiProyecto/core/views.py
+++ MiProyecto/core/views.py
@@ -1,4 +1,7 @@
+from core.forms import VehiculoForm
 from django.shortcuts import render
+from .models import Vehiculo
+from .forms import VehiculoForm #caso alternativo de que el por defecto este mal hecho
 
 # Create your views here.
 def index(request):
@@ -6,3 +9,30 @@ def index(request):
 
 def contacto(request):
     return render(request, 'core/contacto.html')
+
+def listarVehiculos(request):
+    vehiculos = Vehiculo.objects.all()
+
+    datos = {
+        'vehiculos' : vehiculos
+    }
+    return render(request, 'core/listarVehiculos.html',datos)
+
+def agregarVehiculo(request):
+
+    form = VehiculoForm()
+
+    datos = {
+        'form' : VehiculoForm()
+    }
+
+    if request.method == 'POST':
+        formulario = VehiculoForm(request.POST)
+        
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje'] = 'Vehiculo agregado exitosamente'
+
+
+    return render(request, 'core/agregarVehiculo.html',datos)
+

bff1e0a42317b83d7935754361664574c035b06e
Ott021 <ot.pacheco@duocuc.cl>
2021-07-08 00:02:34 -0400

    api

--- MiProyecto/rest/views.py
+++ MiProyecto/rest/views.py
@@ -5,16 +5,17 @@ from rest.serializers import VehiculoSerializer
 from django.shortcuts import render
 from core.models import Vehiculo
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.parsers import JSONParser
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 #GET : listar todos los vehiculos
 #POST : nuevo vehiculo
 
 @csrf_exempt
 @api_view(['GET','POST'])
-
+@permission_classes((IsAuthenticated,))
 def lista_vehiculos(request):
     if request.method == 'GET':
         vehiculos = Vehiculo.objects.all()
